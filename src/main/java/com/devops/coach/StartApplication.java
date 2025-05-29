d77e316148e3a7faf598f91715261809046e4bcb
AzureAD\PawanKumar <pawank071989@gmail.com>
2025-05-29 10:47:52 +0530

    changes mes

--- src/main/java/com/devops/coach/StartApplication.java
+++ src/main/java/com/devops/coach/StartApplication.java
@@ -12,8 +12,8 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "Welcome Pawan.you did it.We are learning Kubernete Deployment using Helm, Jenkins Pipeline Today's date is Jan 18th, 2025");
-        model.addAttribute("msg", "Hello world..We are deploying springboot application into EKS cluster using Helm + Jenkins Pipeline!!!!");
+        model.addAttribute("title", "Welcoe folks..We are learning Kubernete Deployment using Helm, Jenkins Pipeline Today's date is Jan 18th, 2025");
+        model.addAttribute("msg", "Hello All..We are deploying springboot application into EKS cluster using Helm + Jenkins Pipeline!!!!");
         return "index";
     }
 
